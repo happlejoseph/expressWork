589c1aa8ba0034eac4e21d5e81126e41445402b0
happle joseph <happlejoseph66@gmail.com>
2026-06-22 21:54:32 +0530

    created userschema

---
+++ server/model/user.js
@@ -0,0 +1,33 @@
+
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        require: true
+    },
+
+    email: {
+        type: String,
+        require: true
+    },
+
+    phone: {
+        type: String,
+        require: true
+    },
+
+    role: {
+        type: String,
+        require: true
+    },
+
+    password: {
+        type: String,
+        require: true
+    }
+}, {timestamps: true})
+
+const User = mongoose.model('userData', userSchema)
