4d51435b321a9fc7f3a3bfab97574b32de9ae6f8
happle joseph <happlejoseph66@gmail.com>
2026-06-23 02:58:32 +0530

    login compilited

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -44,3 +44,64 @@ export const register = async(req, res, next)=> {
         
     }
 }
+
+
+
+export const login = async(req, res, next)=> {
+    
+    try {
+
+        const {email, password} = req.body
+
+        if(!email) {
+
+            return res.status(401).json({
+                message: 'email is required'
+            });
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user) {
+            return res.status(401).json({
+                message: 'invalid email'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(
+            req.body.password, user.password
+        );
+
+        if(!isMatch) {
+            res.status(401).json({
+                message: 'invalid password'
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                userEmail: user.email
+            },
+
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_TOKEN_EXPIRY
+            }
+        );
+
+        return res.status(200).json({
+            status: true,
+            message: 'successful',
+            data: null,
+            result: user,
+            access_token: token
+        });
+
+
+    }
+    catch(err) {
+        console.log(err);
+        
+    }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,11 @@
 
 
 import Router from "express";
-import { register } from "../controllers/userController.js";
+import { login, register } from "../controllers/userController.js";
 
 const router = Router()
 
 router.post('/register', register)
+router.post('/login', login)
 
 export default router
